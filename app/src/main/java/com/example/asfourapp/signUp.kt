fa44845aae61c27d66a0061b37c6710bf151db5b
chrinore <chrinoren@gmail.com>
2025-12-13 17:49:07 +0100

    Fin du signUp page

--- app/src/main/java/com/example/asfourapp/signUp.kt
+++ app/src/main/java/com/example/asfourapp/signUp.kt
@@ -1,29 +1,20 @@
 package com.example.asfourapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class signUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val registerWay = findViewById<TextView>(R.id.registerText)
-        registerWay.setOnClickListener {
-            val intent = Intent(this, signUp::class.java)
-            startActivity(intent)
-        }
-
     }
 }
