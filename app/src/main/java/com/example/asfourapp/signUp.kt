5db6fbba3506638f30e717aba80f4ee2835a5506
chrinore <chrinoren@gmail.com>
2025-12-16 14:44:36 +0100

    add logique connecion

--- app/src/main/java/com/example/asfourapp/signUp.kt
+++ app/src/main/java/com/example/asfourapp/signUp.kt
@@ -1,6 +1,8 @@
 package com.example.asfourapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class signUp : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val registerWay = findViewById<TextView>(R.id.registerButton)
+        registerWay.setOnClickListener {
+            startActivity(Intent(this, home::class.java))
         }
     }
 }
